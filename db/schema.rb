bc9d472f989d0dc204ac69ccca44c03f12a60808
shariff6 <shariff.mohammed.a@gmail.com>
2017-08-04 14:30:32 +0300

    this commit creates a skills table in our db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804111731) do
+ActiveRecord::Schema.define(version: 20170804112556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 20170804111731) do
     t.string "title"
     t.string "link"
     t.string "technology"
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.integer "admin_id"
+    t.string "skill"
+    t.string "mastery"
   end
 
 end
