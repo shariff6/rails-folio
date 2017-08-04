73205a1c05583616c44547ce0b5b806614be1d69
shariff6 <shariff.mohammed.a@gmail.com>
2017-08-04 14:25:34 +0300

    this commit creates a model for projects

---
+++ app/models/projects.rb
@@ -0,0 +1,7 @@
+class Project < ActiveRecord::Base
+    belongs_to :admin
+    validates :title, :presence => true
+    validates :admin_id :presence => true
+    validates :technology :presence => true
+    validates :link :presence => true
+end
