ada1750f6823a92221ddef7688b46136135aca5f
shariff6 <shariff.mohammed.a@gmail.com>
2017-08-04 14:37:19 +0300

    this commit creates our resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ resources :admin do
+   resources :projects
+ end
+ resources :admin do
+   resources :skills
+ end
 end
