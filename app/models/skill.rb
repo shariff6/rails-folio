f4cd419c06fabaa82bfebf0f45a3d1236f18d639
shariff6 <shariff.mohammed.a@gmail.com>
2017-08-06 23:31:52 +0300

    this commit removes the admin traces from this file

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,4 @@
 class Skill < ActiveRecord::Base
-    belongs_to :admin
-    validates :admin_id, :presence => true
     validates :skill, :presence => true
     validates :mastery, :presence => true
 end
