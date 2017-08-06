c44698c8edebed9c15011a2cb2a2efa33e5d92fd
shariff6 <shariff.mohammed.a@gmail.com>
2017-08-06 18:57:22 +0300

    this commit defines our crete action

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -3,4 +3,18 @@ class SkillsController < ApplicationController
       @admin = Admin.find(params[:admin_id])
       @skill = @admin.skills.new
     end
+    def create
+      @admin = Admin.find(params[:admin_id])
+      @skill = @admin.skills.new(skill_params)
+      if @skill.save
+        redirect_to admin_path(@admin.skills)
+      else
+        render :new
+      end
+    end
+
+    private
+      def skill_params
+        params.require(:skill).permit(:admin_id, :skill, :mastery)
+      end
 end
