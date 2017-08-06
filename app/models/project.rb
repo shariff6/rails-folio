6b8f6cac80754a12438f774ea4b724a177466f73
shariff6 <shariff.mohammed.a@gmail.com>
2017-08-06 17:57:45 +0300

    this commit corrects syntax errors in out project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
     belongs_to :admin
     validates :title, :presence => true
-    validates :admin_id :presence => true
-    validates :technology :presence => true
-    validates :link :presence => true
+    validates :admin_id, :presence => true
+    validates :technology, :presence => true
+    validates :link, :presence => true
 end
