0e900b7bcb45c180f5c7a76264031bc70d2a3db4
shariff6 <shariff.mohammed.a@gmail.com>
2017-08-06 18:56:41 +0300

    this commit corrects syntax errors in out skills model"

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,6 @@
 class Skill < ActiveRecord::Base
     belongs_to :admin
-    validates :admin_id :presence => true
-    validates :skill :presence => true
-    validates :mastery :presence => true
+    validates :admin_id, :presence => true
+    validates :skill, :presence => true
+    validates :mastery, :presence => true
 end
