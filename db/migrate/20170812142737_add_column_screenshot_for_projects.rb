6ee4f4b10312ecd72624c9fb4e8c2a936964a744
shariff6 <shariff.mohammed.a@gmail.com>
2017-08-12 20:58:48 +0300

    this adds a column for storing a description and an image link in pur projects table'

---
+++ db/migrate/20170812142737_add_column_screenshot_for_projects.rb
@@ -0,0 +1,5 @@
+class AddColumnScreenshotForProjects < ActiveRecord::Migration[5.1]
+  def change
+    add_column(:projects, :image, :string)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806200824) do
+ActiveRecord::Schema.define(version: 20170812142737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20170806200824) do
     t.string "title"
     t.string "link"
     t.string "technology"
+    t.string "description"
+    t.string "image"
   end
 
   create_table "skills", force: :cascade do |t|
