d5cd4824b29c9f7d3e276b8b52fe1c13b3e7768a
shariff6 <shariff.mohammed.a@gmail.com>
2017-08-12 20:58:11 +0300

    this commit adds a image and descriptionn validation to our project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ActiveRecord::Base
     validates :title, :presence => true
     validates :technology, :presence => true
     validates :link, :presence => true
+    validates :description, :presence => true
+    validates :image, :presence => true
 end
