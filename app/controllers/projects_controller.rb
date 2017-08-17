bdf310d3ccecf8cce2df5f78112b580f3bd7096e
shariff6 <shariff.mohammed.a@gmail.com>
2017-08-17 11:02:25 +0300

    added a before filter to authenticate only one auser as admin

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  before_action :before_filter
   before_action :authenticate_user!
   def index
     if user_signed_in?
@@ -31,9 +32,18 @@ def show
   render :show
 end
 
+
+
 private
   def project_params
     params.require(:project).permit(:title, :technology, :link, :description, :image)
   end
 
+  def before_filter
+    if user_signed_in?
+      redirect_to root_path unless current_user.email == ('shariff.mohammed.a@gmail.com')
+      else
+        redirect_to new_user_session_path
+    end
   end
+end
