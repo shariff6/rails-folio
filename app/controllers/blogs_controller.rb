7999602912b200aa596baef63c728b5bab4282f2
shariff6 <shariff.mohammed.a@gmail.com>
2017-08-17 10:45:33 +0300

    added a before milter methd t authenticat eonly one email

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
 before_action :authenticate_user!
+before_action :before_filter
   def index
     @blogs = Blog.all
   end
@@ -17,11 +18,21 @@ before_action :authenticate_user!
       flash[:danger] = "Error Saving this blog"
     end
   end
+  def show
+    @blog = Blog.find(params[:id])
+  end
 
 private
 
 def blog_params
   params.require(:blog).permit(:title, :blog)
+end
+def before_filter
+  if user_signed_in?
+    redirect_to root_path unless current_user.email == ('shariff.mohammed.a@gmail.com')
+    else
+      redirect_to new_user_session_path
+  end
 end
 
 end
