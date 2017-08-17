d6203ee8994a7b8aeed2713866dbec8644b2bb63
shariff6 <shariff.mohammed.a@gmail.com>
2017-08-17 11:33:00 +0300

    THIS CREATES A CREATE METHOD FOR creating a comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+def new
+  @blog = Blog.find(params[:blog_id])
+  @comment = @blog.comments.new
+end
+def create
+  @blog = Blog.find(params[:blog_id])
+  @comment = @blog.comments.new(comment_params)
+  if @comment.save
+    redirect_to blog_path(@comment.blog)
+  else
+    render :new
+  end
+end
+
+private
+  def comment_params
+    params.require(:comment).permit(:name,  :comment)
+  end
+end
