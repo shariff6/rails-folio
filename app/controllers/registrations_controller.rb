db7cb107a8e557336f4a0b3caee6306281ddd2db
shariff6 <shariff.mohammed.a@gmail.com>
2017-08-17 11:04:41 +0300

    removed method that allowed registration of only one user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,4 @@
 class RegistrationsController < Devise::RegistrationsController
-  before_action :one_user_registered?, only: [:new, :create]
 
   private
 
@@ -8,12 +7,5 @@ class RegistrationsController < Devise::RegistrationsController
   end
   def account_update_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-  def one_user_registered?
-    if ((User.count == 1) & (user_signed_in?))
-      redirect_to root_path
-    elsif User.count == 1
-      redirect_to new_user_session_path
-    end
   end
 end
