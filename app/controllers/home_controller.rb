e619461efc04e0a508bdec2526eb45948891e7f8
shariff6 <shariff.mohammed.a@gmail.com>
2017-08-17 10:46:08 +0300

    added an admin user method that authenticates one users email only

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,17 @@
 class HomeController < ApplicationController
+  helper_method :admin_user
   def index
     @projects = Project.all
     @skills = Skill.all
     @blogs = Blog.all
     render :index
   end
+
+private
+
+def admin_user
+  if current_user.email == ('shariff.mohammed.a@gmail.com')
+    return true
+  end
+end
 end
