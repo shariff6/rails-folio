87ec50cfe22eb2f2c0af165bda35f63a6affdfec
shariff6 <shariff.mohammed.a@gmail.com>
2017-08-24 09:16:29 +0300

    Added resource table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815230442) do
+ActiveRecord::Schema.define(version: 20170823141700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 20170815230442) do
     t.string "technology"
     t.string "description"
     t.string "image"
+  end
+
+  create_table "referals", force: :cascade do |t|
+    t.string "name"
+    t.string "reference"
+    t.string "institution"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "skills", force: :cascade do |t|
