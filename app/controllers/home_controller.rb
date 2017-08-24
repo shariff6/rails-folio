6c0b6d83c08f368a92f45f4f01ffd6407d88ce62
shariff6 <shariff.mohammed.a@gmail.com>
2017-08-24 09:08:18 +0300

    Adding pagination

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,14 @@
 class HomeController < ApplicationController
   helper_method :admin_user
   def index
+    @referals = Referal.all
     @projects = Project.all
     @skills = Skill.all
-    @blogs = Blog.all
-    render :index
+    @blogs = Blog.all.paginate(:per_page => 2, :page => params[:page])
+    respond_to do |format|
+      format.html
+           format.js
+    end
   end
 
 private
