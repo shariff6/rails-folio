5c4f814cbcc2ecc35f78ca0e3b608f0be7e511c2
shariff6 <shariff.mohammed.a@gmail.com>
2017-08-28 23:49:25 +0300

    removed cooked code and added actual admin roles

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :before_filter
-  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, :only => [:index, :show]
   def index
     if user_signed_in?
           @projects = Project.all
@@ -37,13 +36,5 @@ end
 private
   def project_params
     params.require(:project).permit(:title, :technology, :link, :description, :image)
-  end
-
-  def before_filter
-    if user_signed_in?
-      redirect_to root_path unless current_user.email == ('shariff.mohammed.a@gmail.com')
-      else
-        redirect_to new_user_session_path
-    end
   end
 end
